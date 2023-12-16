690abbf2b19f10d6ce2a55b8c2b0027179c8351b
Raul Madero <127880270+Raul-madero@users.noreply.github.com>
2023-12-16 01:35:37 +0000

    Agregue relationship

--- src/models.py
+++ src/models.py
@@ -7,27 +7,6 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
-
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
-
-    def to_dict(self):
-        return {}
-
 class User(Base):
     __tablename__ = 'users'
     id = Column(Integer, primary_key=True)
@@ -43,19 +22,25 @@ class Post(Base):
     user_id = Column(Integer, ForeignKey('users.id'))
     title = Column(String(30), nullable=False)
     description = Column(String(100), nullable=False)
+    user = relationship(User)
 
 class Like(Base):
     __tablename__ = 'likes'
     id = Column(Integer, primary_key=True)
     post_id = Column(Integer, ForeignKey('posts.id'))
     user_id = Column(Integer, ForeignKey('users.id'))
+    post = relationship(Post)
+    user = relationship(User)
 
 class Comment(Base):
     __tablename__ = 'comments'
     id = Column(Integer, primary_key=True)
     post_id = Column(Integer, ForeignKey('posts.id'))
     user_id = Column(Integer, ForeignKey('users.id'))
-    description = Column(String(100), nullable=True)    
+    description = Column(String(100), nullable=True)
+    post = relationship(Post)
+    user = relationship(User)
+    
 ## Draw from SQLAlchemy base
 try:
     result = render_er(Base, 'diagram.png')
